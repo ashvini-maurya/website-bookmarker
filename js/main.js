c1f8b8c56b1c9f78682a11122f6ec88a054b4e9e
Ashvini Kumar <ashvinikumar45@gmail.com>
2017-07-26 23:03:36 +0530

    local store onClick

--- js/main.js
+++ js/main.js
@@ -7,14 +7,13 @@ function localStoreData(){
 			var websiteName = document.getElementById('websiteName').value;
 			var websiteURL = document.getElementById('websiteURL').value;
 
-
 			var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
 			var regex = new RegExp(expression);
 
 			if (websiteURL.match(regex) && websiteName !== "") {
 			  localStorage.setItem(websiteName, websiteURL);
 			} else {
-			  alert("Please enter valid website Name and URL");
+			  console.log("Please enter valid website Name and URL");
 			}			
 		}
 	}
@@ -25,14 +24,13 @@ function localStoreData(){
 
 
 function getLocalStoredData() {
+	localStoreData();
 	var localStoredWebsiteName = localStorage.getItem(websiteName);
 	var localStoredWebsiteURL = localStorage.getItem(websiteURL);
+	document.getElementById('getWebsiteName').innerHTML = '';
+	document.getElementById('getWebsiteUrl').innerHTML = '';
 
 	Object.keys(localStorage).forEach(function(x) { 
-		//console.log(x, localStorage[x])
-		//document.getElementById('getWebsiteName').innerHTML = x;
-		//document.getElementById('getWebsiteUrl').innerHTML = localStorage[x];
-
 		var name = document.createElement('h4');
 		var url = document.createElement('h4');
 		name.innerHTML = x;
@@ -42,6 +40,5 @@ function getLocalStoredData() {
 	})	
 }
 
-
-
 window.onload = getLocalStoredData;
+document.getElementById('addWebsite').onclick = getLocalStoredData;
